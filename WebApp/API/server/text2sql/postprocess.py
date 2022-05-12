40df581a982df84cbc7a71ded9b8c223f8c15861
Phạm Việt Hoa <83808029+phamviethoa@users.noreply.github.com>
2022-05-12 23:39:24 +0700

    feat: post processing for aggregate syntax

--- WebApp/API/server/text2sql/postprocess.py
+++ WebApp/API/server/text2sql/postprocess.py
@@ -1,6 +1,39 @@
+aggregates = [
+  'SUM',
+  'AVG',
+  'COUNT',
+  'MIN',
+  'MAX'
+]
+
+def fix_aggregate_syntax(sql):
+  is_exist_aggregate = False
+  aggregate_function = None
+  for aggregate in aggregates:
+    if aggregate in sql:
+      is_exist_aggregate = True
+      aggregate_function = aggregate
+
+  if not is_exist_aggregate:
+    return sql
+
+  aggregate_expression_start_pos = sql.find(aggregate_function)
+  aggregate_expression_end_pos = sql.find('FROM') - 1
+
+  aggregate_expression = sql[aggregate_expression_start_pos:aggregate_expression_end_pos]
+  aggregate_arg = aggregate_expression[len(aggregate_function) + 1:]
+
+  sql = sql.replace(aggregate_expression, f'{aggregate_function}({aggregate_arg})')
+
+  return sql
+  
+
+fix_aggregate_syntax(sample)
+
 def default_postprocessing(sql: str) -> str:
     sql = sql.split('|')[-1]
 
-    
+    # Fix syntax error of aggregate function
+    sql = fix_aggregate_syntax(sql)
 
     return sql
