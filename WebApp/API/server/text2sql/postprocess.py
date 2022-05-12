3d8918dc817e49b6eb15199b2ba265be47b7ad8b
Phạm Việt Hoa <83808029+phamviethoa@users.noreply.github.com>
2022-05-12 23:41:32 +0700

    fix: update indent

--- WebApp/API/server/text2sql/postprocess.py
+++ WebApp/API/server/text2sql/postprocess.py
@@ -7,28 +7,26 @@ aggregates = [
 ]
 
 def fix_aggregate_syntax(sql):
-  is_exist_aggregate = False
-  aggregate_function = None
-  for aggregate in aggregates:
-    if aggregate in sql:
-      is_exist_aggregate = True
-      aggregate_function = aggregate
+    is_exist_aggregate = False
+    aggregate_function = None
+    for aggregate in aggregates:
+        if aggregate in sql:
+            is_exist_aggregate = True
+            aggregate_function = aggregate
 
-  if not is_exist_aggregate:
+    if not is_exist_aggregate:
+         return sql
+
+    aggregate_expression_start_pos = sql.find(aggregate_function)
+    aggregate_expression_end_pos = sql.find('FROM') - 1
+
+    aggregate_expression = sql[aggregate_expression_start_pos:aggregate_expression_end_pos]
+    aggregate_arg = aggregate_expression[len(aggregate_function) + 1:]
+
+    sql = sql.replace(aggregate_expression, f'{aggregate_function}({aggregate_arg})')
+
     return sql
 
-  aggregate_expression_start_pos = sql.find(aggregate_function)
-  aggregate_expression_end_pos = sql.find('FROM') - 1
-
-  aggregate_expression = sql[aggregate_expression_start_pos:aggregate_expression_end_pos]
-  aggregate_arg = aggregate_expression[len(aggregate_function) + 1:]
-
-  sql = sql.replace(aggregate_expression, f'{aggregate_function}({aggregate_arg})')
-
-  return sql
-  
-
-fix_aggregate_syntax(sample)
 
 def default_postprocessing(sql: str) -> str:
     sql = sql.split('|')[-1]
